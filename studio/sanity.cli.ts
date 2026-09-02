199fafa386b26d50f8fb8ebd831c568604284f67
Anushrey <anushrey@kanahiku.com>
2026-09-03 00:12:18 +0530

    pushed sanity cli

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,4 +5,5 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? '',
     dataset: process.env.SANITY_STUDIO_DATASET ?? 'production',
   },
+  studioHost: process.env.SANITY_STUDIO_HOSTNAME ?? 'r-and-c-roofing',
 });
